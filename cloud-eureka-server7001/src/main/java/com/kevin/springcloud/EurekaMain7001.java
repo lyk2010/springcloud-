8ed056aac38257bdbbc209cc2500f7407c92b06c
lyk2010 <kevinliu2010@qq.com>
2020-03-13 12:46:26 +0800

    单机Eureka构建

--- cloud-eureka-server7001/src/main/java/com/kevin/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/kevin/springcloud/EurekaMain7001.java
@@ -2,16 +2,14 @@ package com.kevin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * @author kevin
- * @create 2020/3/12
- */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
+
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
+
 }
